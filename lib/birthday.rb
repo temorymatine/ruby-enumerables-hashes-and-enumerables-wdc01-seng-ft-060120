38231986b16219c8db935be4fabda8be4ba54f87
Matine temory <temorymatine@gmail.com>
2020-05-25 19:55:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,5 +7,6 @@
 def happy_birthday(birthday_kids)
   
   
-  birthday_kids.each{|key,value| puts "Happy Birthday"
+  birthday_kids.each{|key,value| puts "Happy Birthday #{key}! You are now #{value} years old!"
+  
 end
